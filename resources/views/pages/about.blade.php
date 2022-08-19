5940de97645ff3cda2265adc9aaebf40c33d76c0
aliouneniome <aliouneniome21@gmail.com>
2022-08-19 15:37:33 +0000

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>About Us | Hello City</title>
-
-    </head>
-    <body>
+@extends('app')
+@section('content')
         
-        <p>Built with &hearts; by KAAY COM.</p>
+    <p>Built with &hearts; by KAAY COM.</p>
 
-        <p><a href="/">Revenir a la page d'acceuil</a></p>
+    <p><a href="/">Revenir a la page d'acceuil</a></p>
 
-        <footer>
-            <p>&copy; Copyright {{ date('Y')}}</p>
-        </footer>
-    </body>
-</html>
+@endsection
