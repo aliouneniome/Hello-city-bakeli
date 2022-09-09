437927ab2175de54dafffcf304c5c73859438453
aliouneniome <aliouneniome21@gmail.com>
2022-09-09 12:04:58 +0000

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,10 +13,8 @@
             @yield('content')
         </main>
 
-        <footer>
-            <p class="text-gray-400">
-                &copy; Copyright {{ date('Y')}} 
-                &middot; <a href="/about-us" class="text-indigo-500 hover:text-indigo-600 underline">About Us</a></p>
-        </footer>
+        @include('layouts/partials/_footer')
+
+        
     </body>
 </html>
---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <p class="text-gray-400">
+        &copy; Copyright {{ date('Y')}} 
+        &middot; <a href="/about-us" class="text-indigo-500 hover:text-indigo-600 underline">About Us</a></p>
+</footer>
